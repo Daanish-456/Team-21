c6959e482ce3ce72f53877e166033f555358024b
Daanish-456 <240216456@aston.ac.uk>
2025-10-14 12:34:53 +0100

    Update Math.java

--- Math.java
+++ Math.java
@@ -9,4 +9,13 @@ Function: This function takes two integers and returns the sum
     public int Add(int x, int y) {
         return x + y;
     }
+    /*********************
+Developer: Daanish Ahmed
+University ID: 240216456
+Function: This function takes two integers, subtracts the second one from the first one, then returns the result
+********************/
+    public int Sub(int x, int y) {
+    return x - y;
+}
+
 }
