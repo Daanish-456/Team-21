f6d24a9e9a34e7cdd83b02e085d8e70fb67cb77c
JagjotC1 <240220671@aston.ac.uk>
2025-10-14 12:24:45 +0100

    Update Math.java

--- Math.java
+++ Math.java
@@ -1,3 +1,12 @@
 public class Math{
     // math functions go here
+    /********************************
+    
+Developer: Jagjot  Chaggar
+University ID: 240220671
+Function: This function takes two integers and returns the sum
+********************************/
+    public int Add(int x, int y) {
+        return x + y;
+    }
 }
