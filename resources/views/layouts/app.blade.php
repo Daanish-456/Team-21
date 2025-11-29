7dd76ddf4b2f2a03646af281d4c6a61acb7de63a
JagjotC1 <240220671@aston.ac.uk>
2025-11-29 22:00:59 +0000

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,20 +11,26 @@
     <link href="https://fonts.googleapis.com/css2?family=Noto+Serif:ital,wght@0,100..900;1,100..900&display=swap"
         rel="stylesheet">
 
-    <link rel="stylesheet" href="{{ asset('assets/css/home.css') }}">
+    {{-- Global styles (body, layout, navbar, footer) --}}
+    <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/components/navbar.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/components/footer.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/components/productcard.css') }}">
+
     @stack('styles')
 </head>
 
 <body>
-
     <header>
         @include('components.navbar')
     </header>
+
     <div class="content-container">
         @yield('content')
     </div>
+
     @include('components.footer')
+
     @stack('scripts')
 </body>
 
