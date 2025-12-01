ff6133a6595f637e02875223b4482e3f3b7f497a
AG6Dev <240194305@aston.ac.uk>
2025-12-01 15:58:55 +0000

    fix some css issues on the home page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 
-head>
+<head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>@yield('title', 'Stone & Soul')</title>
@@ -9,7 +9,7 @@ head>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Serif:ital,wght@0,100..900;1,100..900&display=swap"
-          rel="stylesheet">
+        rel="stylesheet">
 
 
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
