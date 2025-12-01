8a3e3d00c2cad9373fad036e3fa352fcaefd84e7
JagjotC1 <240220671@aston.ac.uk>
2025-12-01 14:22:12 +0000

    Update home.blade.php

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,19 +6,21 @@
     <link rel="stylesheet" href="{{ asset('assets/css/home.css') }}" />
     <link rel="stylesheet" href="{{ asset('assets/css/components/productcard.css') }}" />
 @endpush
-@section('content') <div class="feature">
+@section('content') 
+    <div class="feature">
         <div class="feature-text">
             <h1>Ethically Sourced. Soulfully Crafted.</h1>
             <p>Discover jewellery that honors the earth and elevates your spirit.</p>
         </div>
+        
         <a class="feature-link" href="{{ route('shop') }}">Explore Items</a>
     </div>
     <div class="products">
-        {{-- to eventually get real products from the api --}}
         @include('components.productcard')
         @include('components.productcard')
         @include('components.productcard')
     </div>
+
     <div class="about">
         <img src="{{ asset('assets/images/logo.png') }}" alt="Stone & Soul Logo" class="about-logo" />
         <div class="about-text">
