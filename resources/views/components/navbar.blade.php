f346677a520745ecc42c2891c4e4b5af8f8aa048
JagjotC1 <240220671@aston.ac.uk>
2025-12-01 13:37:56 +0000

    Update navbar.blade.php

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -14,11 +14,11 @@
         <a href="{{ route('contact') }}" class="nav-item">Contact</a>
 
         <div class="nav-user-area">
-            <a href="{{ route('basket') }}" class="nav-icon-link" aria-label="Basket">
-                <img src="{{ asset('assets/svg/cart.svg') }}" class="nav-icon" alt="Basket">
+            <a href="{{ route('basket') }}" aria-label="Basket">
+                <img src="{{ asset('assets/svg/cart.svg') }}" class="nav-icon-link" class="nav-icon" alt="Basket">
             </a>
             <a href="{{ route('account') }}" aria-label="Account">
-                <img src="{{ asset('assets/svg/person-sharp.svg') }}"  alt="User Account">
+                <img src="{{ asset('assets/svg/person-sharp.svg') }}" class="nav-icon-link" alt="User Account">
             </a>
         </div>
     </nav>
