eddd27c8fb3ad2060b5661bd893fae47ea0e4714
Alfie Gayton <240194305@aston.ac.uk>
2025-12-03 16:37:44 +0000

    migrations for category and product

---
+++ database/migrations/2025_12_03_160027_product.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('Product', function(Blueprint $table) {
+            $table->id('ProductID');
+            $table->string('Product_Name', 150);
+            $table->text('Description')->nullable();
+            $table->decimal('Price', 10, 2);
+            $table->integer('Stock');
+            $table->string('Image_URL', 255)->nullable();
+            $table->integer('CategoryID');
+        }); 
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('Product');
+    }
+};
