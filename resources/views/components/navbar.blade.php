d92bfa49bfccc648f610b25320a1dd9cd2aa5a24
Daanish Ahmed <240216456@aston.ac.uk>
2025-12-04 19:55:41 +0000

    adding searchbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -14,6 +14,19 @@
         <a href="{{ route('contact') }}" class="nav-item">Contact</a>
 
         <div class="nav-user-area">
+            
+            {{-- Search Bar --}}
+            <form action="{{ route('search') }}" method="GET" class="nav-search-form">
+                <input type="text" name="q" placeholder="Search products..." class="nav-search-input" required>
+                <button type="submit" class="nav-search-btn">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+                        <circle cx="11" cy="11" r="8"></circle>
+                        <path d="m21 21-4.35-4.35"></path>
+                    </svg>
+                </button>
+            </form>
+
+                {{-- User Account and Basket Icons --}}
             <a href="{{ route('basket') }}" class="nav-item" aria-label="Basket">
                 <img src="{{ asset('assets/svg/cart.svg') }}" class="nav-icon" alt="Basket">
             </a>
