55b7934f5478e1746118eeb2193cc2d605237ced
Daanish Ahmed <240216456@aston.ac.uk>
2025-12-04 19:36:59 +0000

    edits to navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -8,17 +8,17 @@
     <nav class="navbar">
         <a href="{{ route('home') }}" class="nav-item">Home</a>
         <a href="{{ route('shop') }}" class="nav-item">Shop All</a>
-        <a href="{{ route('shop') }}" class="nav-item">Necklaces</a>
-        <a href="{{ route('shop') }}" class="nav-item">Bracelets</a>
-        <a href="{{ route('shop') }}" class="nav-item">Rings</a>
+        <a href="{{ route('shop') }}#necklaces" class="nav-item">Necklaces</a>
+        <a href="{{ route('shop') }}#bracelets" class="nav-item">Bracelets</a>
+        <a href="{{ route('shop') }}#rings" class="nav-item">Rings</a>
         <a href="{{ route('contact') }}" class="nav-item">Contact</a>
 
         <div class="nav-user-area">
-            <a href="{{ route('basket') }}" aria-label="Basket">
-                <img src="{{ asset('assets/svg/cart.svg') }}" class="nav-icon-link" class="nav-icon" alt="Basket">
+            <a href="{{ route('basket') }}" class="nav-item" aria-label="Basket">
+                <img src="{{ asset('assets/svg/cart.svg') }}" class="nav-icon" alt="Basket">
             </a>
-            <a href="{{ route('account') }}" aria-label="Account">
-                <img src="{{ asset('assets/svg/person-sharp.svg') }}" class="nav-icon-link" alt="User Account">
+            <a href="{{ route('account') }}" class="nav-item" aria-label="Account">
+                <img src="{{ asset('assets/svg/person-sharp.svg') }}" class="nav-icon" alt="User Account">
             </a>
         </div>
     </nav>
