4dc46aace98b39635db0621343c88138e41b8cff
Daanish Ahmed <240216456@aston.ac.uk>
2025-12-04 20:22:37 +0000

    added .env file + search function

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,4 @@ Route::get('/shop', [ProductController::class, 'index'])->name('shop');
 Route::redirect('/product', '/shop');
 Route::get('/product/{id}', [ProductController::class, 'show'])->name('product');
 Route::get('/category/{id}', [ProductController::class, 'category'])->name('category');
+Route::get('/search', [ProductController::class, 'search'])->name('search');
