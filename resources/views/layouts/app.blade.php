9936d2c1028450a9d689682ae557d7db9aee2052
Alfie Gayton <240194305@aston.ac.uk>
2025-12-05 00:41:17 +0000

    final changes to about

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>@yield('title', 'Stone & Soul')</title>
 
+    <link rel="icon" type="image/png" href="{{ asset('assets/images/logo/logo-stone-soul.png') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Serif:ital,wght@0,100..900;1,100..900&display=swap"
