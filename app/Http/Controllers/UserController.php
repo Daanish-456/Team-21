a68351eb554a06471d24b2cba51623453bd1ca15
Alfie Gayton <240194305@aston.ac.uk>
2026-02-07 17:32:14 +0000

    now logs you in after registering

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,19 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 
-use App\Models\User;
-
 class UserController extends Controller
 {
-    public function register(Request $request) {
+    public function register(Request $request)
+    {
         $credentials = $request->validate([
             'email' => 'required|email',
             'name' => 'required',
             'password' => 'required|min:8',
-            'confirm_password' => 'required|same:password'
+            'confirm_password' => 'required|same:password',
         ]);
 
         $password_hash = password_hash($credentials['password'], PASSWORD_BCRYPT);
@@ -22,22 +22,27 @@ class UserController extends Controller
         $user = User::create([
             'Email' => $credentials['email'],
             'Name' => $credentials['name'],
-            'Password' => $password_hash
+            'Password' => $password_hash,
         ]);
         $user->save();
+
+        $request->session()->regenerate();
+        $request->session()->put('UserID', $user->UserID);
+        $request->session()->put('UserName', $user->Name);
 
         return redirect('/account');
     }
 
-    public function login(Request $request) {
+    public function login(Request $request)
+    {
         $credentials = $request->validate([
             'email' => 'required|email',
-            'password' => 'required'
+            'password' => 'required',
         ]);
 
         $user = User::where('email', '=', $credentials['email'])->first();
-        if($user) {
-            if(password_verify($credentials['password'], $user->Password)) {
+        if ($user) {
+            if (password_verify($credentials['password'], $user->Password)) {
                 $request->session()->regenerate();
                 $request->session()->put('UserID', $user->UserID);
                 $request->session()->put('UserName', $user->Name);
@@ -45,11 +50,14 @@ class UserController extends Controller
                 return redirect('/account');
             }
         }
+
         return back()->with('error', 'The credentials provided are not correct!');
     }
 
-    public function logout(Request $request) {
+    public function logout(Request $request)
+    {
         $request->session()->invalidate();
+
         return redirect('/');
     }
 }
