4042ee72159dfebdadcd412946074e6b34456e71
jassicak08 <240237510@aston.ac.uk>
2026-03-06 23:18:04 +0000

    Debug contact form submission

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -9,26 +9,7 @@ use App\Models\ContactMessage;
 
 class ContactController extends Controller
 {
-    public function createTicket(Request $request) {
-
-    $values = $request->validate([
-        'name' => 'required',
-        'email' => 'required|email',
-        'message' => 'required'
-    ]);
-
-    ContactMessage::create([
-        'Name' => $values['name'],
-        'Email' => $values['email'],
-        'Message' => $values['message'],
-        'UserID' => session('UserID'),
-        'CreatedAt' => now()
-    ]);
-
-    return redirect()->back()->with(
-        'success',
-        'Thanks for contacting Stone & Soul. Ticket successfully submitted.'
-    );
-
+   public function createTicket(Request $request) {
+    dd($request->all());
 }
 }
