224453d0356ddd80baa9b89914fea453db30e452
jassicak08 <240237510@aston.ac.uk>
2026-03-06 22:58:58 +0000

    Implemented contact form database storage

---
+++ app/Models/ContactMessage.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ContactMessage extends Model
+{
+    protected $table = 'Contact_Message';
+
+    protected $primaryKey = 'MessageID';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'Name',
+        'Email',
+        'Message',
+        'UserID',
+        'CreatedAt'
+    ];
+}
