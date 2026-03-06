f2dcf9f1b833f93cfeea35792ceb46b8d751e800
jassicak08 <240237510@aston.ac.uk>
2026-03-06 23:26:30 +0000

    Fix ContactMessage model configuration

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -10,6 +10,25 @@ use App\Models\ContactMessage;
 class ContactController extends Controller
 {
    public function createTicket(Request $request) {
-    dd($request->all());
+
+    $values = $request->validate([
+        'name' => 'required',
+        'email' => 'required|email',
+        'message' => 'required'
+    ]);
+
+    ContactMessage::create([
+        'Name' => $values['name'],
+        'Email' => $values['email'],
+        'Message' => $values['message'],
+        'UserID' => session('UserID'),
+        'CreatedAt' => now()
+    ]);
+
+    return redirect()->back()->with(
+        'success',
+        'Thanks for contacting Stone & Soul. Ticket successfully submitted.'
+    );
 }
+
 }
