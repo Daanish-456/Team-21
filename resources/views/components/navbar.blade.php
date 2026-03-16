8bd58ab3633788354828506fa69b24f622163ba0
jassicak08 <240237510@aston.ac.uk>
2026-03-16 13:12:48 +0000

    updated by Jassica and Jagjot

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,52 +1,64 @@
- 
-<div class="top-strip">
+<link rel="icon" type="image/png" href="{{ asset('assets/images/logo/logo-stone-soul.png') }}">
 
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+<link href="https://fonts.googleapis.com/css2?family=Noto+Serif:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+
+
+<div class="top-strip">
     Complimentary UK delivery on orders over £50
 </div>
-<div class="nav-div">
-<div class="nav-top-row">
-<div class="nav-left-spacer"></div>
-<a href="{{ route('home') }}" class="nav-img">
-<img src="{{ asset('assets/images/logo.png') }}" alt="Stone & Soul">
-</a>
-<div class="nav-user-area">
-<form action="{{ route('search') }}" method="GET" class="nav-search-form">
-<input type="text" name="q" placeholder="Search" class="nav-search-input">
-<button type="submit" class="nav-search-btn" aria-label="Search">
-<svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 24 24" fill="none"
 
-                        stroke="currentColor" stroke-width="1.8">
-<circle cx="11" cy="11" r="8"></circle>
-<path d="m21 21-4.35-4.35"></path>
-</svg>
-</button>
-</form>
-<a href="{{ route('wishlist') }}" class="icon-link" aria-label="Wishlist">
-<svg xmlns="http://www.w3.org/2000/svg" class="nav-icon" width="19" height="19" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.8">
-<path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
-</svg>
-</a>
-<a href="{{ route('basket') }}" class="icon-link" aria-label="Basket">
-<img src="{{ asset('assets/svg/cart.svg') }}" class="nav-icon-img" alt="Basket">
-</a>
-<a href="{{ route('account') }}" class="icon-link" aria-label="Account">
-<img src="{{ asset('assets/svg/person-sharp.svg') }}" class="nav-icon-img" alt="User Account">
-</a>
-<label class="theme-switch" aria-label="Toggle dark mode">
-<input type="checkbox" id="themeToggle">
-<span class="slider"></span>
-</label>
-</div>
-</div>
-<nav class="navbar">
-<a href="{{ route('home') }}" class="nav-item">Home</a>
-<a href="{{ route('shop') }}" class="nav-item">Shop All</a>
-<a href="{{ route('shop') }}#necklaces" class="nav-item">Necklaces</a>
-<a href="{{ route('shop') }}#earrings" class="nav-item">Earrings</a>
-<a href="{{ route('shop') }}#bracelets" class="nav-item">Bracelets</a>
-<a href="{{ route('shop') }}#rings" class="nav-item">Rings</a>
-<a href="{{ route('about') }}" class="nav-item">Our Story</a>
-<a href="{{ route('contact') }}" class="nav-item">Contact</a>
-</nav>
-</div>
- 
+
+<header class="site-header">
+
+    <div class="header-top">
+
+        <div class="header-left"></div>
+
+        <div class="nav-img">
+            <img src="{{ asset('assets/images/logo.png') }}" alt="Stone & Soul">
+        </div>
+
+        <div class="nav-user-area">
+
+            <form action="{{ route('search') }}" method="GET" class="nav-search-form">
+                <input type="text" name="q" placeholder="Search">
+                <button type="submit">🔍</button>
+            </form>
+
+            <a href="{{ route('wishlist') }}" class="nav-icon">❤</a>
+
+            <a href="{{ route('basket') }}">
+                <img src="{{ asset('assets/svg/cart.svg') }}" class="nav-icon">
+            </a>
+
+            <a href="{{ route('account') }}">
+                <img src="{{ asset('assets/svg/person-sharp.svg') }}" class="nav-icon">
+            </a>
+
+            <label class="theme-switch">
+                <input type="checkbox" id="themeToggle">
+                <span class="slider"></span>
+            </label>
+
+        </div>
+
+    </div>
+
+
+    <nav class="main-nav">
+
+        <a href="{{ route('home') }}">Home</a>
+        <a href="{{ route('about') }}">Our Story</a>
+        <a href="{{ route('shop') }}">Shop All</a>
+        <a href="{{ route('shop') }}#necklaces">Necklaces</a>
+        <a href="{{ route('shop') }}#earrings">Earrings</a>
+        <a href="{{ route('shop') }}#bracelets">Bracelets</a>
+        <a href="{{ route('shop') }}#rings">Rings</a>
+        <a href="{{ route('contact') }}">Contact</a>
+
+    </nav>
+
+</header>
