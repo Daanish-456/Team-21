3e96092a9239bc09a4d948bccbab79c81f8fe39c
jassicak08 <240237510@aston.ac.uk>
2026-03-19 19:09:28 +0000

    Fix product card dynamic images

--- resources/views/components/productcard.blade.php
+++ resources/views/components/productcard.blade.php
@@ -1,6 +1,17 @@
-{{-- to be replaced with backend api stuff, should be clickable which takes you to product listing --}}
 <div class="productcard">
-    <img src="{{ asset('assets/images/example-necklace.png') }}" class="productcard-image" alt="Product Image" />
-    <h1 class="productcard-title">Example Necklace</h1>
-    <p class="productcard-description">Example Product Description</p>
+    <img src="{{ url('website/public/' . $product->Image_URL) }}" 
+         class="productcard-image" 
+         alt="{{ $product->Product_Name }}" />
+
+    <h1 class="productcard-title">
+        {{ $product->Product_Name }}
+    </h1>
+
+    <p class="productcard-description">
+        {{ $product->Description }}
+    </p>
+
+    <p class="productcard-price">
+        £{{ $product->Price }}
+    </p>
 </div>
